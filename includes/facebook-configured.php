7105397dbfc1046fbe7c531fd9cf2e455947db85
Alan Pomerantz <alan_pomerantz@hotmail.com>
2014-05-06 19:18:53 -0300

    Switching online to offline fb

--- includes/facebook-configured.php
+++ includes/facebook-configured.php
@@ -6,15 +6,17 @@ include_once("../includes/facebook.php");
 		'appId'  => '511297335556303',
 		'secret' => '574a1a675f22239be84c587f9dda88e6',
 	));
-$facebooklocal = new Facebook(array(
+   
+
+/*   $facebooklocal = new Facebook(array(
 		'appId'  => '1423275721261751',
 		'secret' => 'fb128bad83e110f49f368737e2885c8f',
 	));
-	
+*/	
 	/* LOCAL FIX - COMMENT ON ONLINE VERSION */
-		Facebook::$CURL_OPTS[CURLOPT_SSL_VERIFYPEER] = false;
-		Facebook::$CURL_OPTS[CURLOPT_SSL_VERIFYHOST] = 2;
+	//	Facebook::$CURL_OPTS[CURLOPT_SSL_VERIFYPEER] = false;
+	//	Facebook::$CURL_OPTS[CURLOPT_SSL_VERIFYHOST] = 2;
 	/* END LOCAL FIX */ 
 	
-$facebook = $facebooklocal;	
+//$facebook = $facebooklocal;	
 	
