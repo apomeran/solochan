a6fa4d5d3376cf86a9a9cec794e971ed0f6cc603
apomeran <alan_pomerantz@hotmail.com>
2014-07-10 16:33:56 -0300

    Finished login Header

--- login-container.php
+++ login-container.php
@@ -2,20 +2,21 @@
 include_once("includes/config.php");
 ?>
 
-<div class="givemeback givemefont">
-    
-    <div class="login">
+<div id="login-container" class="givemeback givemefont">
+
+    <div class="login ">
         <div style="font-size: 20px;">Conectate con tu red social favorita</div>
         <div id="fb-root">
-		</div>
-        <div class="login-social"><button class="btn btn-link" id="login-fb-btn"><img src="img/login-fb.jpg" alt="Iniciar sesi&oacute;n con Facebook"/></button></div>
-        <div class="login-social"><button class="btn btn-link" id="login-li-btn"><img src="img/login-li.jpg" alt="Iniciar sesi&oacute;n con LinkedIn"/></button></div>
-
+        </div>
+        <div>
+            <div class="login-social"><button class="btn btn-link" id="login-fb-btn"><img width="70%" src="img/social/fb2.png" alt="Iniciar sesi&oacute;n con Facebook"/></button></div>
+            <div class="login-social"><button class="btn btn-link" id="login-li-btn"><img width="70%" src="img/social/li2.png" alt="Iniciar sesi&oacute;n con LinkedIn"/></button></div>
+        </div>
     </div>
-	<hr>
+    <hr>
     <div class="login login-not-social">
-        <div style="font-size: 12px;">O inici&aacute; sesi&oacute;n con tu usuario</div>
-        <form id="form-login" >
+        <div style="font-size: 17px; margin-bottom:5px">O inici&aacute; sesi&oacute;n con tu usuario</div>
+        <form id="form-login">
 
             <input name="usuario" class="givemefont" type="text" placeholder="E-mail" id="login-usuario" />
             <input name="clave" class="givemefont" type="password" placeholder="Contrase&ntilde;a" id="login-clave" />
@@ -29,17 +30,18 @@ include_once("includes/config.php");
         </form>
         <div id="form-login-mensaje" class="alert alert-error hide"></div>
     </div>
-	<hr>
+    <hr>
     <div class="login center">
-	   <div>	
-        <div style="font-size: 12px;">
-		&iquest;No ten&eacute;s una cuenta? 
-		</div>
-		<a class="btn btn-link givemefont" rel="address:/usuario-nuevo" href="#/editar-usuario"><b>Registrate GRATIS</b></a>
-	   </div>	
+        <div>	
+            <div style="font-size: 12px;">
+                &iquest;No ten&eacute;s una cuenta? 
+            </div>
+            <a class="btn btn-link givemefont" rel="address:/usuario-nuevo" href="#/editar-usuario"><b>Registrate GRATIS</b></a>
+        </div>	
     </div>
 
 </div>
+
 <!-- <div class="columna-imagen">
         <img src="img/columna.png" alt="" />
 </div> -->
