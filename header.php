772c3e5f7262da5984712d423e6750723ef0f35c
Alan Pomerantz <alan_pomerantz@hotmail.com>
2014-07-10 23:54:24 -0300

    Added bw effects

--- header.php
+++ header.php
@@ -12,6 +12,7 @@
         <link type="text/css" rel="stylesheet" href="css/bootstrap.css" />
         <link type="text/css" rel="stylesheet" href="css/jquery-ui.css" />
         <link type="text/css" rel="stylesheet" href="css/styles.css" />
+		<link rel="stylesheet" href="css/image-effects.css"/>
         <link rel="shortcut icon" href="favicon.ico" />
         <link rel="apple-touch-icon" href="img/apple-touch-icon-iphone.png" />
         <link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-ipad.png" />
@@ -41,14 +42,14 @@
                 <div class="span6" style="margin-left:9%">
                     <div class="row">
                         <div class="span6">
-                            <a href="#/inicio" rel="address:/" class="logo"><img width=43% src="img/logo2c.png" alt="SoloChanguitas" /></a>
+                            <a href="#/inicio" rel="address:/" class="logo bw"><img width=43% src="img/logo2c.png" alt="SoloChanguitas" /></a>
                             <div class="slogan givemefont">
                                 <div class="social" style="">
                                     Trabajos por los que and&aacute;s preguntando
-                                    <a href="http://www.facebook.com/solochanguitas" style="margin-left:3px;" target="_blank"><img width=6% src="img/social/fb.png" alt="facebook" /></a>
-                                    <a href="https://twitter.com/solochanguitas" target="_blank"><img  width=6% src="img/social/tw.png" alt="twitter" /></a>
-                                    <a href="#" target="_blank"><img width=6% src="img/social/g+.png" alt="google plus" /></a>
-                                    <a href="#" target="_blank"><img width=6% src="img/social/li.png" alt="linkedin" /></a>
+                                    <a class="bw" href="http://www.facebook.com/solochanguitas" style="margin-left:3px;" target="_blank"><img width=6% src="img/social/fb.png" alt="facebook" /></a>
+                                    <a class="bw" href="https://twitter.com/solochanguitas" target="_blank"><img  width=6% src="img/social/tw.png" alt="twitter" /></a>
+                                    <a class="bw" href="#" target="_blank"><img width=6% src="img/social/g+.png" alt="google plus" /></a>
+                                    <a class="bw" href="#" target="_blank"><img width=6% src="img/social/li.png" alt="linkedin" /></a>
                                 </div>
                             </div>
 
