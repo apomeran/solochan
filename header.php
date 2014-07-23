27bcd89a21d7ce0c87aaba4305f6d480801e6d6b
apomeran <alan_pomerantz@hotmail.com>
2014-07-23 17:05:11 -0300

    New Header

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
         <link type="text/css" rel="stylesheet" href="css/bootstrap.css" />
         <link type="text/css" rel="stylesheet" href="css/jquery-ui.css" />
         <link type="text/css" rel="stylesheet" href="css/styles.css" />
-		<link rel="stylesheet" href="css/image-effects.css"/>
+        <link rel="stylesheet" href="css/image-effects.css"/>
         <link rel="shortcut icon" href="favicon.ico" />
         <link rel="apple-touch-icon" href="img/apple-touch-icon-iphone.png" />
         <link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-ipad.png" />
@@ -28,76 +28,29 @@
             <![endif]-->
     </head>
     <script>
-
         function scroll(element, parent) {
             $(parent).animate({scrollTop: $(parent).scrollTop() + $(element).offset().top - $(parent).offset().top}, {duration: 'slow', easing: 'swing'});
             $('html,body').animate({scrollTop: $(parent).offset().top}, {duration: 1000, easing: 'swing'});
         }
-
     </script>
     <body class="body-bg fadein" style="background-color: rgb(237, 241, 240);">
-
         <div id="header">
-            <div class="row">
-                <div class="span6" style="margin-left:9%">
-                    <div class="row">
-                        <div class="span6">
-                            <a href="#/inicio" rel="address:/" class="logo bw"><img width=43% src="img/logo2c.png" alt="SoloChanguitas" /></a>
-                            <div class="slogan givemefont">
-                                <div class="social" style="">
-                                    Trabajos por los que and&aacute;s preguntando
-                                    <a class="bw" href="http://www.facebook.com/solochanguitas" style="margin-left:3px;" target="_blank"><img width=6% src="img/social/fb.png" alt="facebook" /></a>
-                                    <a class="bw" href="https://twitter.com/solochanguitas" target="_blank"><img  width=6% src="img/social/tw.png" alt="twitter" /></a>
-                                    <a class="bw" href="#" target="_blank"><img width=6% src="img/social/g+.png" alt="google plus" /></a>
-                                    <a class="bw" href="#" target="_blank"><img width=6% src="img/social/li.png" alt="linkedin" /></a>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-
-                <div class="span6" style="position: absolute; right: 5.5%; margin-top: -56px;">
-                    <p class="como-f">
-                       
-                        <span id="userPanel">
-                            <?php
-                            if (!isset($_SESSION[SesionId]) || $_SESSION[SesionId] == 0)
-                                include_once("panel-login.php");
-                            else
-                                include_once("panel-logged.php");
-                            ?>
-                        </span>
-                    </p>
-                </div>
+            <div class="logo">
+                <a href="#/inicio" rel="address:/" class="bw">
+                    <img width=55% src="img/logo2.png" alt="SoloChanguitas" />
+                </a>
+            </div>
+            <div class="como-f">
+                <span id="userPanel">
+                    <?php
+                    if (!isset($_SESSION[SesionId]) || $_SESSION[SesionId] == 0)
+                        include_once("panel-login.php");
+                    else
+                        include_once("panel-logged.php");
+                    ?>
+                </span>
             </div>
         </div>
         <div class="content">
-            <div id="slider" class="slider">
-                <div class="slider-pic">
-                    <img src="img/slider/1.png"/>
-                    <div style="margin: 0 auto;
-                         background-color: rgba(0,0,0,0.5);
-                         height: 90px; position: absolute;
-                         z-index: 1;
-                         bottom: 0;
-                         width: 80%;">
-                        <div class="givemefont" style="margin: 0 auto;
-                             float: none;
-                             font-size: 23px;
-                             font-weight: 1500;
-                             color: #FFFFFF;
-                             width: 100%;
-                             padding-top: 10px; text-align:center">
-
-                            <p>Public&aacute; ese trabajo por el que and&aacute;s preguntando,</p><p>&iexcl;vas a tener postulados en cuesti&oacute;n de horas!</p>
-
-                        </div>
-
-                    </div>
-                </div>
-
-            </div>
-
-            <div class="container-div" >
+            <div class="container-div">
 
